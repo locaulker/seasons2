a54dcde1a8f499a5f6f2796835bbac80744669ea
Lawrence <Caulker>
2020-06-23 17:13:42 -0400

    addedd state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import SeasonDisplay from "./SeasonDisplay"
 
-// functin based component
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  )
-
-  return <div>Latitude: </div>
-}
 // Class-based component
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    )
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <div>Loading...</div>
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"))
